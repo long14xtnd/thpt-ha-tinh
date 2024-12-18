16d9fcdc26d2e7db8080b61fd4b5ac7d73cfc7bd
long14xtnd <longdoan14xtnd@gmail.com>
2024-12-18 17:24:35 +0700

    sua lai anh

--- skin_vr.js
+++ skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.0.9/20024
 // Filename: feather_vr.ggsk
-// Generated 2024-12-14T10:43:52
+// Generated 2024-12-18T17:14:09
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
